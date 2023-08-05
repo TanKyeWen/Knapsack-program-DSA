e0e8923c177c47b255759c077a3d309c73bfa571
imjhanC <chengjiahan@outlook.com>
2023-08-05 13:31:04 +0800

    with messages

--- src/main_interface.java
+++ src/main_interface.java
@@ -1,5 +1,5 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public interface main_interface{
+    // our main idea is to use interface > abstract > class > main class to test the application   
+    public int method1();
+    public void method2();
 }
