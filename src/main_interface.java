90517b3c0a8eb599e4c7f15bcb4f80a05d21d97d
imjhanC <jhan9502@gmail.com>
2023-08-06 14:01:43 +0800

    e

--- src/main_interface.java
+++ src/main_interface.java
@@ -1,6 +1,6 @@
 public interface main_interface{
     // our main idea is to use interface > abstract > class > main class to test the application   
-    public int method1();
+    public void method1();
     public void method2();
     public void method3();
 }
