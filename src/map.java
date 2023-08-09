720b73c77d0d79c2845cf35c3638512d706759d4
ShyJun <zhanjunlee123@gmail.com>
2023-08-09 18:59:15 +0800

    T1

--- src/map.java
+++ src/map.java
@@ -2,23 +2,23 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class map {
-    private Map<String, Integer> myPrivateMap; //private instance of  map
+    private Map<String, Double, Double> myPrivateMap; //private instance of  map
     
 
     public map() {
         myPrivateMap = new HashMap<>(); //constructor
     }
 
-    public Map<String, Integer> getMyPrivateMap() { //getter
+    public Map<String, Double, Double> getMyPrivateMap() { //getter
         return myPrivateMap;
     }
 
-    public void setMyPrivateMap(Map<String, Integer> myPrivateMap) { //setter
+    public void setMyPrivateMap(Map<String, Double, Double> myPrivateMap) { //setter
         this.myPrivateMap = myPrivateMap;
     }
 
-    public void addToMap(String key, Integer value) { //add to map
-        myPrivateMap.put(key, value);
+    public void addToMap(String key, Double weight, Double value) { //add to map
+        myPrivateMap.put(key, weight, value);
     }
 
     public void printMapContents(){ //print map contents
