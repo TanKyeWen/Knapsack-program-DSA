80aa08e346f342c55d759e14bbe65e82f6f8d94b
imjhanC <jhan9502@gmail.com>
2023-08-09 20:27:57 +0800

    abstraction class update

--- src/AbstractionTestOnly.java
+++ src/AbstractionTestOnly.java
@@ -1,8 +1,8 @@
 public class AbstractionTestOnly{
     public static void main(String[] args){
        ConcreteAbstraction ab = new ConcreteAbstraction();
-        ab.put("Key1", 10.5, 20.3); //inserter
-        ab.put("Key2", 15.2, 30.7); //inserter
+        ab.put("Key1", 10.5, 20.3);     //inserter
+        ab.put("Key2", 15.2, 30.7);     //inserter
         ab.printMapValues();  //print values
     }
 }
