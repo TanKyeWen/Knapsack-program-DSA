5fbbb1bbc2e08a5366c68970097dcb541dc00e77
imjhanC <jhan9502@gmail.com>
2023-08-19 12:44:17 +0800

    deleted the original interface

--- src/main_interface.java
+++
@@ -1,6 +0,0 @@
-public interface main_interface{
-    // our main idea is to use interface > abstract > class > main class to test the application   
-    public void method1();
-    public void method2();
-    public void method3();
-}
