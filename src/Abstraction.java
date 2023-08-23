1dec6b741fa8e9581febefb7973591d35e2ee1e6
imjhanC <jhan9502@gmail.com>
2023-08-23 08:21:23 +0800

    l

--- src/Abstraction.java
+++ src/Abstraction.java
@@ -1,12 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public abstract class Abstraction implements Interface{
     protected Map<String, TripleValues> map;  //private instance for Map
